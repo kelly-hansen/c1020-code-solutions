1570db1d6045a02c16a25ee906c73d2c21a880a0
Kelly Hansen <kellyhansen182@gmail.com>
2020-10-12 15:19:55 -0700

    Completed javascript-operators-and-expressions | Kelly Hansen

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+var width = 12;
+var height = 8;
+var area = width * height;
+
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 22;
+var payment = 67;
+var change = payment - bill;
+
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 69;
+var midterm = 99;
+var final = 82;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+var firstName = 'Kelly';
+var lastName = 'Hansen';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName: ', fullName);
+console.log('typof fullName: ', typeof fullName);
+
+var pH = 25;
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 44;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
