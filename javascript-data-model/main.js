efde1b8f923adf531935da6c10bb586fa950c0ee
Kelly Hansen <kellyhansen182@gmail.com>
2020-10-13 10:46:42 -0700

    Completed javascript-data-model | Kelly Hansen

---
+++ javascript-data-model/main.js
@@ -0,0 +1,60 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    orderNum: '114-3941689-8772232',
+    totalPrice: 34.00,
+    orderDate: '2020-08-04',
+    productList: [
+      {
+        productName: 'Javascript for impatient programmers',
+        productPrice: 31.55
+      }
+    ],
+    delivered: true,
+    returnWindow: false
+  },
+  {
+    orderNum: '113-9984268-1280257',
+    totalPrice: 44.53,
+    orderDate: '2020-07-19',
+    productList: [
+      {
+        productName: 'The Timeless Way of Building',
+        productPrice: 41.33
+      }
+    ],
+    delivered: true,
+    returnWindow: false
+  },
+  {
+    orderNum: '114-2875557-9059409',
+    totalPrice: 17.22,
+    orderDate: '2020-07-04',
+    productList: [
+      {
+        productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        productPrice: 15.98
+      }
+    ],
+    delivered: true,
+    returnWindow: false
+  },
+  {
+    orderNum: '113-2883177-2648248',
+    totalPrice: 138.93,
+    orderDate: '2020-07-03',
+    productList: [
+      {
+        productName: 'GameCube Controller - Super Smash Brose. Edition (Nintendo Switch)',
+        productPrice: 94.95
+      },
+      {
+        productName: 'The Art of Sql',
+        productPrice: 33.99
+      }
+    ],
+    delivered: true,
+    returnWindow: false
+  }
+];
