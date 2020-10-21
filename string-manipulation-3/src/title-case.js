20677c541cb4b34e5e2f90b6981a244147cb8a40
Kelly Hansen <kellyhansen182@gmail.com>
2020-10-21 16:00:43 -0700

    Completed string-manipulation-3 | Kelly Hansen

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -29,6 +29,62 @@ function removeSemicolon(string) {
   return stringWSemicolon;
 }
 
+function specialCases(array) {
+  for (var s = 0; s < array.length; s++) {
+    if (array[s] === 'javascript') {
+      array[s] = 'JavaScript';
+    }
+    if (array[s] === 'api') {
+      array[s] = 'API';
+    }
+  }
+}
+
+function titleAndSub(array) {
+  if (array.length > 0) {
+    var firstWord = array[0].split('');
+    firstWord[0] = firstWord[0].toUpperCase();
+    array[0] = firstWord.join('');
+  }
+}
+
+var minorWordsArr = [
+  'and',
+  'or',
+  'nor',
+  'but',
+  'a',
+  'an',
+  'the',
+  'as',
+  'at',
+  'by',
+  'for',
+  'in',
+  'of',
+  'on',
+  'per',
+  'to'
+];
+
+function majorWords(array) {
+  if (array.length > 0) {
+    for (var t = 0; t < array.length; t++) {
+      var isMinorWord = false;
+      for (var m = 0; m < minorWordsArr.length; m++) {
+        if (array[t] === minorWordsArr[m]) {
+          isMinorWord = true;
+        }
+      }
+      if (!isMinorWord) {
+        var splitWord = array[t].split('');
+        splitWord[0] = splitWord[0].toUpperCase();
+        array[t] = splitWord.join('');
+      }
+    }
+  }
+}
+
 function titleCase(string) {
   var splitString = string.split(' ');
   for (var i = 0; i < splitString.length; i++) {
@@ -51,8 +107,17 @@ function titleCase(string) {
       title.push(splitString[q]);
     }
   }
-
-  console.log('splitString: ', splitString);
-  console.log('title: ', title);
-  console.log('subTitle: ', subTitle);
+  specialCases(title);
+  specialCases(subTitle);
+  titleAndSub(title);
+  titleAndSub(subTitle);
+  majorWords(title);
+  majorWords(subTitle);
+  var titleString = title.join(' ');
+  var result = titleString;
+  if (subTitle.length > 0) {
+    var subTitleString = ': ' + subTitle.join(' ');
+    result += subTitleString;
+  }
+  return result;
 }
