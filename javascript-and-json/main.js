618e85e0f666908bba1d13cd64239a5cb9105d12
Kelly Hansen <kellyhansen182@gmail.com>
2020-10-23 13:03:11 -0700

    Completed javascript-and-json | Kelly Hansen

---
+++ javascript-and-json/main.js
@@ -0,0 +1,31 @@
+var booksArr = [
+  {
+    isbn: '123',
+    title: 'Huckleberry Finn',
+    author: 'Mark Twain'
+  },
+  {
+    isbn: '456',
+    title: 'Harry Potter',
+    author: 'JK Rowling'
+  },
+  {
+    isbn: '789',
+    title: 'The Shining',
+    author: 'Stephen King'
+  }
+];
+
+console.log('booksArr:', booksArr);
+
+var booksStr = JSON.stringify(booksArr);
+console.log('booksStr:', booksStr);
+console.log('typeof booksStr:', typeof booksStr);
+
+var studentJSON = '{"id":123456,"name":"Ron Weasley"}';
+console.log('studentJSON:', studentJSON);
+console.log('typeof studentJSON:', typeof studentJSON);
+
+var studentObj = JSON.parse(studentJSON);
+console.log('studentObj:', studentObj);
+console.log('typeof studentObj:', typeof studentObj);
