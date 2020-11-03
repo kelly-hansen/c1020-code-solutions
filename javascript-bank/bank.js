77845353ccd758dfe933b110947b3635a5e61e0b
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-03 14:54:37 -0800

    Completed javascript-bank | Kelly Hansen

---
+++ javascript-bank/bank.js
@@ -0,0 +1,33 @@
+/* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0) {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  }
+  return null;
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var total = 0;
+  for (var x = 0; x < this.accounts.length; x++) {
+    total += this.accounts[x].getBalance();
+  }
+  return total;
+};
