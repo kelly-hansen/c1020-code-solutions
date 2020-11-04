fb82f3d5b4b336644cf83d06c45178476d171716
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-04 15:42:27 -0800

    DOM render based on images array contents

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,8 +1,9 @@
 var $displayedImg = document.querySelector('img');
-var $indicators = document.querySelectorAll('.indicator');
 var $carousel = document.querySelector('.carousel-cont');
+var $indicatorsCont = document.querySelector('.indicators-cont');
 
-var images = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+// images array must have at least one item to render carousel properly
+var images = ['https://i.pinimg.com/originals/12/e7/66/12e766676ab8e08a904be3a72b9e7615.png', 'images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
 
 var currentImgIndex = 0;
 
@@ -19,6 +20,7 @@ function switchImage(providedIndex) {
     newImgIndex = images.length - 1;
   }
   $displayedImg.setAttribute('src', images[newImgIndex]);
+  var $indicators = document.querySelectorAll('.indicator');
   $indicators[currentImgIndex].removeAttribute('id');
   $indicators[newImgIndex].id = 'selected';
   currentImgIndex = newImgIndex;
@@ -32,7 +34,7 @@ function handleCarouselClick(e) {
   } else if (e.target.className === 'fas fa-chevron-left') {
     switchImage(currentImgIndex - 1);
   } else if (e.target.className === 'indicator') {
-    var targetIndex = e.target.getAttribute('index');
+    var targetIndex = e.target.getAttribute('data-index');
     switchImage(parseInt(targetIndex));
   }
 }
@@ -40,3 +42,15 @@ function handleCarouselClick(e) {
 $carousel.addEventListener('click', handleCarouselClick);
 
 var imgStepInterval = setInterval(switchImage, 3000);
+
+function renderCarousel() {
+  $displayedImg.setAttribute('src', images[0]);
+  for (var i = 1; i < images.length; i++) {
+    var $indicatorDiv = document.createElement('div');
+    $indicatorDiv.className = 'indicator';
+    $indicatorDiv.setAttribute('data-index', i);
+    $indicatorsCont.appendChild($indicatorDiv);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', renderCarousel);
