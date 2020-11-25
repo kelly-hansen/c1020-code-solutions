0544e8a82287e2b4ce9cbda9f4ead3eab10b539b
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-25 10:45:05 -0800

    Completed node-module-system | Kelly Hansen

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+console.log('typeof exports:', typeof exports);
+console.log('exports:', exports);
+
+console.log('typeof require:', typeof require);
+console.log('require:', require);
+
+console.log('typeof module:', typeof module);
+console.log('module:', module);
+
+console.log('typeof _filename:', typeof __filename);
+console.log('_filename:', __filename);
+
+console.log('typeof _dirname:', typeof __dirname);
+console.log('_dirname:', __dirname);
