b59664ed715da0a4865cf25fadc42733654cc4f4
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-04 15:34:42 -0800

    post request complete

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -35,9 +35,22 @@ app.post('/api/grades', (req, res) => {
   }
   if (!Number.isInteger(newScore) || newScore < 1 || newScore > 100) {
     res.status(400).json({ error: 'score must be an integer 1-100' });
-
+    return;
   }
-
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+         values ($1, $2, $3)
+      returning *;
+  `;
+  const values = [newName, newCourse, newScore];
+  db.query(sql, values)
+    .then(result => {
+      res.status(201).json(result.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error has occurred' });
+    });
 });
 
 app.listen(3000, () => {
