f4c4f5b0795986f104aa3acb0d9530d1892ce859
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-04 12:42:53 -0800

    Create new pg database connection object

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const pg = require('pg');
 
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:lfz@localhost/studentGradeTable'
+});
+
 const app = express();
