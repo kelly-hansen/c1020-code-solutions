0e5a33dcf760c57aa470b1bc33372d2098993611
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-04 13:21:51 -0800

    Get request for full grades table

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -7,3 +7,23 @@ const db = new pg.Pool({
 });
 
 const app = express();
+
+app.get('/api/grades', (req, res) => {
+  const sql = `
+    select *
+      from "grades";
+  `;
+  db.query(sql)
+    .then(result => {
+      const grades = result.rows;
+      if (!grades) {
+        res.status(500).json({ error: 'An unexpected error has occurred' });
+      } else {
+        res.status(200).json(grades);
+      }
+    });
+});
+
+app.listen(3000, () => {
+  process.stdout.write('Express server listening on port 3000\n');
+});
