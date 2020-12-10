4b25f4340904728000cd20e567f8d466eebb30a3
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-10 13:31:11 -0800

    Switch working

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
 export default class ToggleSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { on: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const newSwitchStatus = !this.state.on;
+    this.setState({ on: newSwitchStatus });
+  }
+
   render() {
+    let contClass;
+    if (this.state.on) {
+      contClass = 'cont on';
+    } else {
+      contClass = 'cont off';
+    }
+
     return (
-      <div>Hello World!</div>
+      <div className={contClass} onClick={this.handleClick}>
+        <div className="switch"></div>
+      </div>
     );
   }
 }
