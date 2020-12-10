0678ce1eadf5629d72255c7eb7122278d8d096de
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-10 11:53:42 -0800

    Completed react-hot-button-component | Kelly Hansen

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+let clickCount = 0;
+const buttonColors = ['gray', 'purple', 'lightpurple', 'red', 'orange', 'yellow', 'white'];
+let currentButtonColorIndex = 0;
+
 export default class HotButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      class: buttonColors[currentButtonColorIndex]
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    clickCount++;
+    if (clickCount === 3 && currentButtonColorIndex < buttonColors.length - 1) {
+      clickCount = 0;
+      currentButtonColorIndex++;
+      this.setState({
+        class: buttonColors[currentButtonColorIndex]
+      });
+    }
+  }
 
   render() {
     return (
-      <button>Hot Button</button>
+      <button className={this.state.class} onClick={this.handleClick}>Hot Button</button>
     );
   }
 }
