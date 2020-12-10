7dccca8403baad4321629c7b3e5e2bfa467466ae
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-10 15:51:28 -0800

    Stopwatch counting up

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -4,15 +4,48 @@ import ReactDOM from 'react-dom';
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      running: false,
+      time: 0,
+      timerID: null
+    };
+    this.playPause = this.playPause.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
+  playPause() {
+    const stateCopy = this.state;
+    if (this.state.running) {
+      stateCopy.running = false;
+      clearInterval(stateCopy.timerID);
+      this.setState(stateCopy);
+    } else {
+      stateCopy.running = true;
+      stateCopy.timerID = setInterval(() => {
+        stateCopy.time++;
+        this.setState(stateCopy);
+      }, 1000);
+      this.setState(stateCopy);
+    }
+  }
+
+  reset() {}
+
   render() {
+    let playPauseIcon;
+    if (!this.state.running) {
+      playPauseIcon = 'fas fa-play';
+    } else {
+      playPauseIcon = 'fas fa-pause';
+    }
     const newStopwatch = (
       <div className="cont">
         <div className="watch">
-          <p>0</p>
+          <p>{this.state.time}</p>
         </div>
-        <div className="play-pause"><i className="fas fa-play"></i></div>
+        <div className="play-pause" onClick={this.playPause}>
+          <i className={playPauseIcon}></i>
+        </div>
       </div>
     );
 
