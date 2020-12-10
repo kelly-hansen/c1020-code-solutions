881be3e8095e34aa4db9d8edbc85c1c74e49dc04
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-10 12:17:29 -0800

    Refactored HotButton to contain clickCount in state

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
 
-let clickCount = 0;
-const buttonColors = ['gray', 'purple', 'lightpurple', 'red', 'orange', 'yellow', 'white'];
-let currentButtonColorIndex = 0;
-
 export default class HotButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      class: buttonColors[currentButtonColorIndex]
+      clickCount: 0
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    clickCount++;
-    if (clickCount === 3) {
-      clickCount = 0;
-      currentButtonColorIndex++;
-      this.setState({
-        class: buttonColors[currentButtonColorIndex]
-      });
-    }
+    this.setState({
+      clickCount: this.state.clickCount + 1
+    });
   }
 
   render() {
     let button;
-    if (buttonColors[currentButtonColorIndex] === 'white') {
-      button = <button className={this.state.class}>Hot Button</button>;
+    if (this.state.clickCount < 3) {
+      button = <button className="gray" onClick={this.handleClick}>Hot Button</button>;
+    } else if (this.state.clickCount < 6) {
+      button = <button className="purple" onClick={this.handleClick}>Hot Button</button>;
+    } else if (this.state.clickCount < 9) {
+      button = <button className="lightpurple" onClick={this.handleClick}>Hot Button</button>;
+    } else if (this.state.clickCount < 12) {
+      button = <button className="red" onClick={this.handleClick}>Hot Button</button>;
+    } else if (this.state.clickCount < 15) {
+      button = <button className="orange" onClick={this.handleClick}>Hot Button</button>;
+    } else if (this.state.clickCount < 18) {
+      button = <button className="yellow" onClick={this.handleClick}>Hot Button</button>;
     } else {
-      button = <button className={this.state.class} onClick={this.handleClick}>Hot Button</button>;
+      button = <button className="white">Hot Button</button>;
     }
     return (
       button
