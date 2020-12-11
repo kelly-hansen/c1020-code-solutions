82dac9f37df12f233beaa8612e8f74de361436b0
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-11 11:17:45 -0800

    Completed react-form-controls | Kelly Hansen

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    event.preventDefault();
+    console.log('state:', this.state);
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" onChange={this.handleChange} value={this.state.email} />
+        <button type="submit" onClick={this.handleSubmit}>Submit</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.getElementById('root')
+);
