8d7f084a98bb57a8cf02bc0e62245fe9552d51ff
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-12 13:49:06 -0800

    Rendering properly based on state

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,8 +4,10 @@ class AccordionItem extends React.Component {
   render() {
     return (
       <div>
-        <div className="title">Working</div>
-        {this.props.selected && <div className="details">Details</div>}
+        <div className="title">{this.props.itemObj.title}</div>
+        {this.props.selectedId === this.props.itemObj.id && (
+          <div className="details">{this.props.itemObj.details}</div>
+        )}
       </div>
     );
   }
@@ -20,17 +22,11 @@ class Accordion extends React.Component {
   }
 
   render() {
-    let list;
-    if (this.state.selected === null) {
-      list = (
-        <div>
-          <AccordionItem selected="true" />
-          <AccordionItem />
-          <AccordionItem />
-        </div>
-      );
-    }
-    return list;
+    const list = this.props.items.map(item => {
+      return <AccordionItem selectedId={this.state.selected} itemObj={item} key={item.id} />;
+    });
+
+    return <div>{list}</div>;
   }
 }
 
