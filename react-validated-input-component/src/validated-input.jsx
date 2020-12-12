1c8dcc9e8be1d402527021885eb9e9df5ebe6061
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-11 16:47:19 -0800

    JSX and CSS layout

---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+
+  }
+
+  render() {
+
+    return (
+      <form>
+        <label htmlFor="password">Password</label>
+        <div>
+          <input
+            type="password"
+            id="password"
+            onChange={this.handleChange}
+            value={this.state.password} />
+          <i className="fas fa-check"></i>
+        </div>
+        <label htmlFor="password" name="validation">Validation</label>
+      </form>
+    );
+  }
+}
+
+export default ValidatedInput;
