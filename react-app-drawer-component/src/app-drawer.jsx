3d31bc03febf1e7582ec476f04c346c717359d7a
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-11 16:01:50 -0800

    Use toggleDrawer() to show/hide menu based on state

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -4,17 +4,25 @@ class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: true
+      isOpen: false
     };
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+  }
+
+  toggleDrawer() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+
   }
 
   render() {
     let drawer;
     if (this.state.isOpen === false) {
-      drawer = <i className="fas fa-bars"></i>;
+      drawer = <i className="fas fa-bars" onClick={this.toggleDrawer}></i>;
     } else {
       const menuItems = this.props.menuItems.map(item => {
-        return <p key={item.id}>{item.name}</p>;
+        return <p key={item.id} onClick={this.toggleDrawer}>{item.name}</p>;
       });
       drawer = (
         <div className="modal">
@@ -22,7 +30,7 @@ class AppDrawer extends React.Component {
             <h2>Menu</h2>
             {menuItems}
           </div>
-          <div className="dark-bg"></div>
+          <div className="dark-bg" onClick={this.toggleDrawer}></div>
         </div>
       );
     }
