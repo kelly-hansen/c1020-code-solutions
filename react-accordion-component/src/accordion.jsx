09b1c5831826441da962a92437c8cecd93753a84
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-12 12:56:34 -0800

    Initial accordion.jsx structure

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
+
+  render() {
+    let list;
+
+    return list;
+  }
+}
+
+export default Accordion;
