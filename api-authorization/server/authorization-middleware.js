d255d4500474482933d86f42987e5d1299d9548d
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-17 12:02:22 -0800

    Completed api-authorization | Kelly Hansen

--- api-authorization/server/authorization-middleware.js
+++ api-authorization/server/authorization-middleware.js
@@ -4,8 +4,11 @@ const ClientError = require('./client-error'); // eslint-disable-line
 function authorizationMiddleware(req, res, next) {
   const token = req.headers['x-access-token'];
   if (!token) {
-
+    throw new ClientError(401, 'authentication required');
   }
+  const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+  req.user = payload;
+  next();
   /* your code here */
   /**
    * Try to get the 'X-Access-Token' from the req.headers.
